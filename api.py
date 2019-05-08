273e075346a793a73804b57c9a6e36d6ddbcf6fc
Harrison Turton <harrisonturton@gmail.com>
2019-05-09 00:37:24 +1000

    Named api constructor __main__ instead of __init__

--- api.py
+++ api.py
@@ -2,11 +2,11 @@ from slackclient import SlackClient
 
 class Client:
     # Create a new client instance
-    def __main__(self, bot_access_token):
+    def __init__(self, bot_access_token):
         self.client = SlackClient(bot_access_token);
     # Send a message to some channel
     def message(self, channel, message):
-        slack_client.api_call(
+        self.client.api_call(
             "chat.postMessage",
             channel=channel,
             text=message
